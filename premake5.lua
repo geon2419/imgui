983dade53ff155de9e673fdd7b28deb26bc74a34
geon2419 <geonhyeoklee.kr@gmail.com>
2026-02-01 15:01:58 +0900

    fix: remove unused layout deletion in SetActiveID; update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,34 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
-    staticruntime "off"
+kind "StaticLib"
+language "C++"
+staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
+files
+{
+"imconfig.h",
+"imgui.h",
+"imgui.cpp",
+"imgui_draw.cpp",
+"imgui_internal.h",
+"imgui_tables.cpp",
+"imgui_widgets.cpp",
+"imstb_rectpack.h",
+"imstb_textedit.h",
+"imstb_truetype.h",
+"imgui_demo.cpp",
+"backends/imgui_impl_opengl3.cpp",
+"backends/imgui_impl_opengl3.h",
+"backends/imgui_impl_opengl3_loader.h",
+}
+
+includedirs
+{
+".",
+"../GLFW/include"
+}
 
 	filter "system:windows"
 		systemversion "latest"
